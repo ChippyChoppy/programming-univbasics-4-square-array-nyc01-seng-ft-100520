223e812750c1fb9666d778d4902dbed108b7e0ec
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-27 03:16:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  counter = 0 
+  numbers = [1, 2, 3]
+  new_numbers = [ ]
+  
+  while numbers[counter] do 
+    puts numbers[counter]
+    counter = counter ** 2 
+    new_numbers.push(numbers[counter])
+  end
+ 
 end
